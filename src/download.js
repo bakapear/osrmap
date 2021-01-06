e503a41159db57a1d16728745700dc74715afe12
bakapear <proxybomb@gmail.com>
2021-01-06 17:55:20 +0100

    use sayobot instead of bloodcat

--- src/download.js
+++ src/download.js
@@ -1,40 +1,33 @@
 let fs = require('fs')
-let https = require('https')
 let path = require('path')
+let https = require('https')
 
-async function main () {
-  let file = process.argv[2]
-  if (!file || !file.endsWith('.osr')) return console.error('Please input an osu! replay file.')
+let TYPE = 'mini'
+
+async function main (replay) {
+  if (!replay || !replay.endsWith('.osr')) return console.error('Please input an osu! replay file.')
+
+  let map = await getMapFromReplay(replay)
+  if (!map) return console.error('Could not find beatmap from replay.')
+
+  let name = `${map.beatmapset_id} ${map.artist} - ${map.title}`.replace(/[/?<>\\:*|"]/g, '')
+
+  let file = await downloadMap(map.beatmapset_id, (current, total) => {
+    write(`Downloading "${name}" ${((current * 100) / total).toFixed(2)}%`)
+  })
+  fs.writeFileSync(path.join(path.dirname(replay), name) + '.osz', file, { encoding: 'binary' })
+}
+
+async function getMapFromReplay (file) {
   let replay = fs.readFileSync(file, { encoding: 'binary' })
   let hash = replay.substr(replay.indexOf(' ') + 2, 32)
   let map = await get('https://ripple.moe/api/get_beatmaps?h=' + hash)
-  map = JSON.parse(map)[0]
-  if (!map) return console.error('Could not find beatmap from replay.')
-  let name = `${map.beatmapset_id} ${map.artist} - ${map.title}`
-  let osu = await get('https://bloodcat.com/osu/_data/beatmaps/' + map.beatmapset_id + '.osz', 'binary', (current, total) => {
-    write(`Downloading "${name}" ${((current * 100) / total).toFixed(2)}%`)
-  })
-  name = name.replace(/[/?<>\\:*|"]/g, '')
-  fs.writeFileSync(path.join(path.dirname(file), name) + '.osz', osu, { encoding: 'binary' })
+  return JSON.parse(map)[0]
 }
 
-main()
-
-function get (url, encoding, progress) {
-  return new Promise((resolve, reject) => {
-    https.get(url, res => {
-      if (encoding) res.setEncoding(encoding)
-      let total = Number(res.headers['content-length'])
-      let cur = 0
-      let body = ''
-      res.on('data', function (chunk) {
-        body += chunk
-        cur += chunk.length
-        if (progress) progress(cur, total)
-      })
-      res.on('end', () => resolve(body))
-    }).on('error', reject)
-  })
+async function downloadMap (set, progress) {
+  let map = await get(`https://txy1.sayobot.cn/beatmaps/download/${TYPE}/${set}`, 'binary', progress)
+  return map
 }
 
 function write (msg) {
@@ -42,3 +35,23 @@ function write (msg) {
   process.stdout.clearLine()
   process.stdout.write(msg)
 }
+
+async function get (url, encoding, progress) {
+  return new Promise((resolve, reject) => {
+    https.get(url, async res => {
+      if (res.statusCode >= 300 && res.statusCode < 400) {
+        return resolve(get(res.headers.location, encoding, progress))
+      }
+      if (encoding) res.setEncoding(encoding)
+      let total = res.headers['content-length'] || 0
+      let data = ''
+      res.on('data', d => {
+        data += d
+        if (progress) progress(data.length, total)
+      })
+      res.on('end', () => resolve(data))
+    })
+  })
+}
+
+main(process.argv[2])
